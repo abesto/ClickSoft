649f507fea4729bfd149c320bbad0b2aa0040118
Zoltan Nagy <abesto@abesto.net>
2015-07-20 22:43:04 +0200

    Refactor ThingController

--- Assets/Scripts/ThingController.cs
+++ Assets/Scripts/ThingController.cs
@@ -1,82 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-public class ThingController : MonoBehaviour {	
-	abstract class ThingState {
-		protected ThingController controller;
-		public ThingState(ThingController controller) {
-			this.controller = controller;
-			Enter ();  // Not nice having this here, but this is a throw-away state machine, so it's fine
-		}
-
-		protected bool IsHovered() {
-			return HoverUtil.Instance.IsHovered (controller.gameObject);
-		}
-
-		virtual protected void Enter() {}
-
-		virtual public ThingState Act() { return this; }
+abstract class ThingState {
+	protected ThingController controller;
+	public ThingState(ThingController controller) {
+		this.controller = controller;
+		Enter ();  // Not nice having this here, but this is a throw-away state machine, so it's fine
 	}
 	
-	class NormalState: ThingState {
-		public NormalState(ThingController controller): base(controller) {}
+	protected bool IsHovered() {
+		return HoverUtil.Instance.IsHovered (controller.gameObject);
+	}
+	
+	virtual protected void Enter() {}
+	
+	virtual public ThingState Act() { return this; }
+}
 
-		override protected void Enter() {
-			controller.SetColorPreservingAlpha (controller.initialColor);
-			controller.progressText.color = Color.white;
-		}
-
-		override public ThingState Act () {
-			if (IsHovered()) {
-				return new HighlightedState (controller);
-			} else {
-				return this;
-			}
+class NormalState: ThingState {
+	public NormalState(ThingController controller): base(controller) {}
+	
+	override protected void Enter() {
+		controller.ResetColor ();
+		controller.progressTextColor = Color.white;
+	}
+	
+	override public ThingState Act () {
+		if (IsHovered()) {
+			return new HighlightedState (controller);
+		} else {
+			return this;
 		}
 	}
+}
 
-	class HighlightedState: ThingState {
-		public HighlightedState(ThingController controller): base(controller) {}
-
-		override protected void Enter() {
-			controller.SetColorPreservingAlpha (Color.cyan);
-			controller.progressText.color = Color.cyan;
+class HighlightedState: ThingState {
+	public HighlightedState(ThingController controller): base(controller) {}
+	
+	override protected void Enter() {
+		controller.color = Color.cyan;
+		controller.progressTextColor = Color.cyan;
+	}
+	
+	override public ThingState Act () {
+		if (!IsHovered()) {
+			return new NormalState (controller);
 		}
-
-		override public ThingState Act () {
-			if (!IsHovered()) {
-				return new NormalState (controller);
-			}
-			if (!Input.GetMouseButtonDown (0)) {
-				return this;
-			}
-			ToolManager.Instance.tool.Act (controller);
-			if (controller.IsDone ()) {
-				return new DoneState (controller);
-			} else {
-				return this;
-			}
+		if (!Input.GetMouseButtonDown (0)) {
+			return this;
+		}
+		ToolManager.Instance.tool.Act (controller);
+		if (controller.done) {
+			return new DoneState (controller);
+		} else {
+			return this;
 		}
 	}
+}
 
-	class DoneState: ThingState {
-		public DoneState(ThingController controller): base(controller) {}
-
-		override protected void Enter() {
-			controller.SetColorPreservingAlpha (Color.green);
-			controller.progressText.color = Color.green;
-			controller.EmitDoneParticles ();
-		}
+class DoneState: ThingState {
+	public DoneState(ThingController controller): base(controller) {}
+	
+	override protected void Enter() {
+		controller.color = Color.green;
+		controller.progressTextColor = Color.green;
+		controller.EmitDoneParticles ();
 	}
+}
 
+public class ThingController : MonoBehaviour {	
 	public ParticleSystem prefabDoneSparks;
 	public TextMesh prefabProgressText;
 	public byte progressOnClick = 10;
 	public byte initialProgress = 10;
 
 	private ParticleSystem myDoneSparks;
-	private TextMesh progressText;
+	private TextMesh progressTextMesh;
 	private MeshRenderer thing;
 	private Color initialColor;
 	private ThingState state;
@@ -89,37 +88,48 @@ public class ThingController : MonoBehaviour {
 				value = 100;
 			}
 			_progress = value;
-			SetAlpha (value / 100f);
-			SetProgressText (value);
+			alpha = value / 100f;
+			progressText = value;
+		}
+	}
+		
+	private int progressText {
+		set { progressTextMesh.text = value.ToString() + "%"; }
+	}
+	
+	private float alpha {
+		get { return color.a; }
+		set {
+			Color c = color;
+			c.a = value;
+			color = c;
 		}
 	}
 
-	private Color color {
+	public Color color {
 		get { return thing.materials [0].color; }
-		set { thing.materials [0].color = value; }
+		set { 
+			value.a = color.a;
+			thing.materials [0].color = value; 
+		}
 	}
+
+	public void ResetColor() {
+		color = initialColor;
+	}
+
+	public Color progressTextColor {
+		get { return progressTextMesh.color; }
+		set { progressTextMesh.color = value; }
+	}
+
+	public bool done {
+		get { return progress == 100; }
+	}
+
 	
-	bool IsDone() {
-		return progress == 100;
-	}
-
-	void EmitDoneParticles() {
+	public void EmitDoneParticles() {
 		myDoneSparks.Play ();
-	}
-
-	void SetAlpha(float alpha) {
-		Color c = color;
-		c.a = alpha;
-		color = c;
-	}
-
-	void SetProgressText(int value) {
-		progressText.text = value.ToString() + "%";
-	}
-
-	void SetColorPreservingAlpha(Color to) {
-		to.a = color.a;
-		color = to;
 	}
 
 	void Start () {
@@ -129,9 +139,9 @@ public class ThingController : MonoBehaviour {
 		myDoneSparks.transform.SetParent (transform);
 		myDoneSparks.transform.localScale = new Vector3 (1, 1, 1);
 		// Initialize Progress text
-		progressText = Instantiate (prefabProgressText, transform.position, prefabProgressText.transform.rotation) as TextMesh;
-		progressText.transform.SetParent (transform);
-		progressText.transform.Translate (Vector3.up * transform.localScale.z / 2, Space.World);
+		progressTextMesh = Instantiate (prefabProgressText, transform.position, prefabProgressText.transform.rotation) as TextMesh;
+		progressTextMesh.transform.SetParent (transform);
+		progressTextMesh.transform.Translate (Vector3.up * transform.localScale.z / 2, Space.World);
 		// Set initial progress
 		progress = initialProgress;
 		initialColor = color;
