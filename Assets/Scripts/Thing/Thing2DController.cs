8e5dccc8fbb8436c36cc3d3c407ae6fc6b83d1cb
Zoltan Nagy <abesto@abesto.net>
2015-08-08 23:45:07 +0200

    Experimenting with 2D UI

---
+++ Assets/Scripts/Thing/Thing2DController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thing2DController : MonoBehaviour {
+	public SpriteRenderer greenInsideRenderer;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float cutoff = Mathf.InverseLerp (0, Screen.width, Input.mousePosition.x);
+		greenInsideRenderer.material.SetFloat("_Cutoff", cutoff); 
+	}
+}
