3869888b274a92a9965d8894a1698291b01369c7
Zsolt Dollenstein <zsol.zsol@gmail.com>
2015-08-29 18:00:20 +0200

    tab?

--- Assets/Developer.cs
+++ Assets/Developer.cs
@@ -6,57 +6,57 @@ using System.Collections;
 public class Developer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
-    const float MIN_COMMITS_PER_DAY = 0.1f;
-    const float MAX_COMMITS_PER_DAY = 6f;
-    float commitsPerSecond = 0.5f;
-    public ThingController target = null;
-    float timeSinceLastWork = 0f;
-    float nextCommit = 0f;
+	const float MIN_COMMITS_PER_DAY = 0.1f;
+	const float MAX_COMMITS_PER_DAY = 6f;
+	float commitsPerSecond = 0.5f;
+	public ThingController target = null;
+	float timeSinceLastWork = 0f;
+	float nextCommit = 0f;
 
-    public float commitsPerDay {
-        get {
-            return (float)System.Math.Round(commitsPerSecond * GameTime.SECONDS_PER_DAY, 1);
-        }
-    }
+	public float commitsPerDay {
+		get {
+			return (float)System.Math.Round(commitsPerSecond * GameTime.SECONDS_PER_DAY, 1);
+		}
+	}
 
-    // Use this for initialization
-    void Start ()
-    {
-        commitsPerSecond = Random.Range(MIN_COMMITS_PER_DAY, MAX_COMMITS_PER_DAY) / GameTime.SECONDS_PER_DAY;
-        determineNextCommit();
-        Debug.Log (string.Format("Developer created with {0} efficiency", commitsPerSecond.ToString()));
-        transform.Find("Efficiency").GetComponent<Text>().text = string.Format("{0} / day", commitsPerDay.ToString());
-        MoniesController.Instance.deltaCash -= commitsPerSecond * 100;
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		commitsPerSecond = Random.Range(MIN_COMMITS_PER_DAY, MAX_COMMITS_PER_DAY) / GameTime.SECONDS_PER_DAY;
+		determineNextCommit();
+		Debug.Log (string.Format("Developer created with {0} efficiency", commitsPerSecond.ToString()));
+		transform.Find("Efficiency").GetComponent<Text>().text = string.Format("{0} / day", commitsPerDay.ToString());
+		MoniesController.Instance.deltaCash -= commitsPerSecond * 100;
+	}
 
-    // Update is called once per frame
-    void Update ()
-    {
-        if (target != null) {
-            timeSinceLastWork += Time.deltaTime;
-            if (timeSinceLastWork >= nextCommit) {
-	            target.commitWork ();
-                timeSinceLastWork = 0f;
-                determineNextCommit();
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (target != null) {
+			timeSinceLastWork += Time.deltaTime;
+			if (timeSinceLastWork >= nextCommit) {
+				target.commitWork ();
+				timeSinceLastWork = 0f;
+				determineNextCommit();
+			}
+		}
+	}
 
-    void determineNextCommit() {
-        nextCommit = Random.Range(0f, 2f) / commitsPerSecond;
-    }
+	void determineNextCommit() {
+		nextCommit = Random.Range(0f, 2f) / commitsPerSecond;
+	}
 
-    public void OnPointerEnter (PointerEventData eventData)
-    {
-        if (target != null) {
-            target.HighlightOn(ThingHighlightSource.MENU);
-        }
-    }
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		if (target != null) {
+			target.HighlightOn(ThingHighlightSource.MENU);
+		}
+	}
 
-    public void OnPointerExit (PointerEventData eventData)
-    {
-        if (target != null) {
-            target.HighlightOff(ThingHighlightSource.MENU);
-        }
-    }
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		if (target != null) {
+			target.HighlightOff(ThingHighlightSource.MENU);
+		}
+	}
 }
